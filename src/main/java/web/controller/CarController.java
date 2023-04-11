6860e0b6fc3f88716bbfd2521f6e46e4765f7159
osivaM <o.v.v.9322@gmail.com>
2023-04-11 18:02:50 +0500

    second commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,19 +1,26 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImp;
+import web.service.CarService;
 
 @Controller
 public class CarController {
 
+    private CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping(value = "/cars")
     public String getCar(@RequestParam(value = "count", defaultValue = "5") String count, ModelMap model) {
-        CarServiceImp carServiceImp = new CarServiceImp();
 
-        model.addAttribute("messages", carServiceImp.getCars(Integer.parseInt(count)));
+        model.addAttribute("messages", carService.getCars(Integer.parseInt(count)));
 
         return "cars";
     }
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,10 +1,12 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class CarServiceImp implements CarService {
     private List<Car> list;
 
